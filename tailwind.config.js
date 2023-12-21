49e81771d83da295f9fb45c0de84adf9d3982a75
SaammeeH <62329819+SaaammmyyyS@users.noreply.github.com>
2023-12-21 20:17:35 +0800

    [Home] Add Article section and Blogs section (#3)
    * Added Blogs and Events Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 import forms from "@tailwindcss/forms";
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -12,11 +13,13 @@ export default {
 
     theme: {
         colors: {
+            ...colors,
             primary: "#000000",
             secondary: "#000000",
             pink: "#EB14BC",
             purple: "#AF14EB",
-            "light-purple": "#CF72F3",
+            "light-purple": "#D4D3F8",
+            "lighter-purple" : "#D4D3F8",
             "dark-purple": "#46085E",
             "footer-bg": "#27282B",
         },
